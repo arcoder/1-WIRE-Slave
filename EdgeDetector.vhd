b5d936810d87800387112953d08271ad4be98739
Alberto Ruffo <alberto.ruffo90@gmail.com>
2013-04-15 13:37:04 +0300

    Update EdgeDetector.vhd

--- EdgeDetector.vhd
+++ EdgeDetector.vhd
@@ -1,8 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: Alberto Ruffo
--- 
--- Create Date:    12:09:46 11/25/2012 
 -- Design Name: 
 -- Module Name:    EdgeDetector - Behavioral 
 -- Project Name: 
@@ -20,15 +18,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity EdgeDetector is port(
 	BUS_1WIRE: in std_logic;
 	UP:out std_logic;
@@ -40,7 +29,7 @@ end EdgeDetector;
 
 architecture Behavioral of EdgeDetector is
 
-signal Q: std_logic_vector (15 downto 0) := (others => '0');
+signal Q: std_logic_vector (1 downto 0) := (others => '0');
 signal Q_LAST, temp, temp2: std_logic := '0';
 
 begin
@@ -58,7 +47,7 @@ if( RESET = '1') then
 	Q <= (others => '0');     
 	Q_LAST <= '0';
 elsif( CLK'event and CLK = '1' ) then 
-	Q <= BUS_1WIRE & Q(15 downto 1);
+	Q <= BUS_1WIRE & Q(1);
 	Q_LAST <= Q(0);
 end if;  
 end process;
